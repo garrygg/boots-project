fd2b33cb35666d4268ac4acbdf2027f084327066
garrygg <569014973@qq.com>
2019-02-21 23:45:26 +0800

    nothing

--- boots-shiro/src/main/java/gg/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package gg.dao;
-
-import gg.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-	User findUserByLoginName(String loginName);
-}
